05d2baa4351f875430a282d6f8f33f3c7b3b901a
Matthew Mettler <matthewrmettler@gmail.com>
2015-09-26 05:36:45 -0400

    Got League pulling and match list working. Started working on readme.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # zenith-blade-analysis
 Repo containing code written for my blog, Zenith Blade
+
+#Data collection
+In order to look at or analyze the data, we need to get it, first! For basically any project moving forward,
+my approach follows these steps:
+1.) Populate the league table with the seed IDs
+2.) Sample random users in the league table and get their matchlist
+3.) Sample their matchlist, get a list of summoner IDs from those matches
+4.) Sample those match summoner IDs, use their IDs to append rows to the league table
+
+In general, the 'league table' is stored longterm (as in, for the life of that project, as this table changes every second). The actual match history or matchlist data is not saved.
--- id_collection.R
+++ id_collection.R
@@ -1,59 +1,100 @@
 library(httr)
 library(jsonlite)
 
-#Bronze: 21938826
-#Silver: 46590181
-#Gold: 22497997
-#Platinum: 23316933
-#Diamond: 19859333
-seed_ids = c(21938826, 46590181, 22497997, 23316933, 19859333)
-api_key <- readLines(con <- file("key.txt", "r"))
+#Seed IDs
+bronze_seed_id = 21938826
+silver_seed_id = 46590181
+gold_seed_id = 22497997
+platinum_seed_id = 23316933
+diamond_seed_id = 19859333
+seed_ids = c(bronze_seed_id, silver_seed_id, gold_seed_id, platinum_seed_id, diamond_seed_id)
 
-GetLeagueBySummoner <- function(region, summoner.id) {
-  url <- paste("https://", region, ".api.pvp.net/api/lol/", region, 
-               "/v2.5/league/by-summoner/", summoner.id, "?api_key=", api_key, 
-               sep="")
-  req <- GET(url)
-  
-  ## Check status code for errors
-  if (status_code(req) == 200) {
-    return(fromJSON(content(req, "text")))
-    
+#Load API key
+api_key <- readLines(con <- file("key.txt", "r"))
+close(con)
+
+CheckStatus <- function(api_call, url) {
+  if (status_code(api_call) == 200) {
+    return(TRUE)
   } else {
     if (status_code(req) == 400) {
       # Bad syntax, the url is wrong
       print("Response code 400, fix the url!")
-      return(-1)
+      return(FALSE)
       
     } else if (status_code(req) == 401) {
       # No authorization, bad api key
       print("Response code 401, bad API key / no key provided!")
-      return(-1)
+      return(FALSE)
       
     } else if (status_code(req) == 404) {
       # Summoner not found!
       print("Response code 404, summoner not found!")
-      return(0)
+      return(FALSE)
       
     } else if (status_code(req) == 429) {
       print("Response code 429, rate limit exceeded! 
             Trying again in 1.0 seconds...")
       Sys.sleep(1.0)
-      return(GetLeague(region, summoner.id))
+      return(CheckStatus(GET(url), url))
       
     } else if (status_code(req) == 500) {
       print("Response code 500, internal server error! 
             Try again later?")
-      return(-1)
+      return(FALSE)
       
     } else if (status_code(req) == 503) {
       print("Response code 503, service unavailable!
             Try again later?")
-      return(-1)
+      return(FALSE)
     } 
   }
 }
 
-raw_data <- GetLeagueBySummoner("na", seed_ids[1])
-data = raw_data[[1]]
-entries = data[[4]][[1]]
+# Returns a data frame containing the contents of the 'get league by summoner'
+# API call. Data frame also includes the tier and region of that call.
+GetLeagueBySummoner <- function(region, summoner.id) {
+  url <- paste("https://", region, ".api.pvp.net/api/lol/", region, 
+               "/v2.5/league/by-summoner/", summoner.id, "?api_key=", api_key, 
+               sep="")
+  req <- GET(url)
+
+  ## Check status code for errors
+  if (CheckStatus(req)) {
+    raw_data <- fromJSON(content(req, "text"))
+    named_data <- raw_data[[1]]
+    solo_q <- named_data[1,]
+    tier <- solo_q$tier
+    entries <- solo_q$entries[[1]]
+    data <- cbind(entries, tier)
+    shuffled_data <- cbind(region, data[,1:2], data[,12], data[,3:10])
+    names(shuffled_data)[4] <- "tier"
+    rownames(shuffled_data) <- NULL
+    return(shuffled_data)
+  }
+  return(list())
+}
+
+GetMatchListBySummoner <- function(region, summoner.id) {
+  url <- paste("https://", region, ".api.pvp.net/api/lol/", region, 
+               "/v2.2/matchlist/by-summoner/", summoner.id, "?api_key=", api_key, 
+               sep="")
+  req <- GET(url)
+  
+  ## Check status code for errors
+  if (CheckStatus(req)) {
+    data <- fromJSON(content(req, "text"))
+    
+    return(data[[1]])
+  }
+  return(FALSE)
+}
+
+league.table <- list()
+for(id in seed_ids) {
+  temprow <- GetLeagueBySummoner("na", id)
+  rownames(league.table) <- rownames(temprow) <- NULL
+  league.table <- rbind(league.table, GetLeagueBySummoner("na", id))
+}
+
+test <- league.table[sample(nrow(league.table), 20), ]
